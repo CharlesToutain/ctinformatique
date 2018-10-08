36d7934b8202df7ff29b11e0e6bbdaf064018a63
Toutain <charles-edouard.toutain@outlook.fr>
2018-10-08 22:10:58 +0200

    first

--- meta/config.js
+++ meta/config.js
@@ -2,21 +2,20 @@
  * Created by vaibhav on 31/3/18
  */
 module.exports = {
-  siteTitle: 'Gatsby Starter Business', // Site title.
-  siteTitleAlt: 'Business', // Alternative site title for SEO.
+  siteTitle: 'CTinformatique | Création de site web et dépannage informatique.', // Site title.
+  siteTitleAlt: 'Informatique,Web, Création de site internet, informaticien, basse-normandie,chanu.', // Alternative site title for SEO.
   siteLogo: '/logos/logo-512x512.png', // Logo used for SEO and manifest.
-  siteUrl: 'https://gatsby-starter-business.netlify.com', // Domain of your website without pathPrefix.
+  siteUrl: 'https://ctinformatique.fr', // Domain of your website without pathPrefix.
   pathPrefix: '/', // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-business/.
-  siteDescription: 'Leverage Gatsby Business Starter for your Business.', // Website description used for RSS feeds/meta description tag.
+  siteDescription: 'CTinformatique indépendant en création de site internet, webconseil, assistance et dépannage informatique.', // Website description used for RSS feeds/meta description tag.
   siteRss: '/rss.xml',
   siteFBAppID: '', // FB Application ID for using app insights
   googleTagManagerID: '', // GTM tracking ID.
-  disqusShortname: 'gatsby-business-starter', // Disqus shortname.
-  userName: 'Vaibhav Sharma',
-  userTwitter: 'vaibhaved',
-  userLocation: 'Delhi NCR, India',
-  userDescription: '',
-  copyright: 'Copyright © Gatsby Starter Business 2018. All Rights Reserved.', // Copyright string for the footer of the website and RSS feed.
-  themeColor: '#00d1b2', // Used for setting manifest and progress theme colors.
+  disqusShortname: 'https-ctinformatique-fr-2', // Disqus shortname.
+  userName: 'Charles-Edouard Toutain',
+  userLocation: 'Flers 61100, Basse-Normandie',
+  userDescription: 'Charles-Edouard Toutain développeur web full stack. Freelance et actuellement en poste chez Subteno-it en alternance en tant que développeur odoo.',
+  copyright: 'Copyright © CTinformatique 2018. All Rights Reserved.', // Copyright string for the footer of the website and RSS feed.
+  themeColor: '#ED1E79', // Used for setting manifest and progress theme colors.
   backgroundColor: '#ffffff', // Used for setting manifest background color.
 }
